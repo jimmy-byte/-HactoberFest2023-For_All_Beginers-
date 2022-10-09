ff4cf118efcb99abf97aa8e7d98ecf9536bd2b67
ꦤꦤꦁ​​ꦲꦫꦶꦥ꦳ꦸꦢꦶꦤ꧀ <nanangarif404@gmail.com>
2022-10-09 15:49:06 +0700

    personal data program

---
+++ personaldata.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+//MARK: membuat form data diri
+print("Welcome to my program")
+print("Enter your first name:"); let firstName = readLine()!
+print("Enter your last name:"); let lastName=readLine()!
+let fullName = firstName + " " + lastName
+let address = "Yogyakarta"
+let job = "iOS Developer"
+let age = 0b10001
+print("----------------------------------")
+print("\(fullName) is a \(job)")
+print("Currently he is \(age) and lives at \(address).")
+
+print("----------------------------------")
