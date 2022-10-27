f6cf1ede28895e087da48addceb21c4922ca361a
Joel Benny <92745721+jo6538@users.noreply.github.com>
2022-10-27 20:42:43 +0530

    NEW COMMIT

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+//#include<conio>
+#include <iostream>
+using namespace std;
+//#include<stdio>
+int main()
+{ 
+    int a,b,s;
+    std::cout<<"SUM OF TWO NUMBERS";
+    std::cout<<"Enter two numbers";
+    cin>>a>>b;
+    s=a+b;
+    cout<<"SUM="<<s;
+    return 0;
+}
