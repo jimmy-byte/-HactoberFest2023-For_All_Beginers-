0ad8a10ba2042add220ab8506facd86fa9fee8cc
Dheeraj Jha <106474979+Dheerajjha451@users.noreply.github.com>
2023-10-01 17:22:20 +0530

    Create ButterflyPattern.java

---
+++ ButterflyPattern.java
@@ -0,0 +1,36 @@
+
+class ButterflyPattern {
+    public static void butter(int n) {
+        // Print the first half of the butterfly pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Print the second half of the butterfly pattern
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        butter(4);
+    }
+}
