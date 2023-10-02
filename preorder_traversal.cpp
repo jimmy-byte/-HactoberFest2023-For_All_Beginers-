2fbf6aafdb1211983bd0c14f4790b772c7706578
vishisht-dubey <vishisht.dubey.phe21@itbhu.ac.in>
2023-10-02 17:38:10 +0530

    added preorder traversal of binry tree

--- preorder_traversal.cpp
+++ preorder_traversal.cpp
@@ -1,49 +1,51 @@
-#include <iostream>  
-  
-using namespace std;  
-  
-struct node {  
-    int element;  
-    struct node* left;  
-    struct node* right;  
-};  
-  
-/*To create a new node*/  
-struct node* createNode(int val)  
-{  
-    struct node* Node = (struct node*)malloc(sizeof(struct node));  
-    Node->element = val;  
-    Node->left = NULL;  
-    Node->right = NULL;  
-  
-    return (Node);  
-}  
-  
-  
-/*function to traverse the nodes of binary tree in preorder*/  
-void traversePreorder(struct node* root)  
-{  
-    if (root == NULL)  
-        return;  
-    cout<<" "<<root->element<<" ";  
-    traversePreorder(root->left);  
-    traversePreorder(root->right);  
-}  
-  
-int main()  
-{  
-    struct node* root = createNode(39);  
-    root->left = createNode(29);  
-    root->right = createNode(49);  
-    root->left->left = createNode(24);  
-    root->left->right = createNode(34);  
-    root->left->left->left = createNode(14);  
-    root->left->left->right = createNode(27);  
-    root->right->left = createNode(44);  
-    root->right->right = createNode(59);  
-    root->right->right->left = createNode(54); 
-      
-    cout<<"\n The Preorder traversal of given binary tree is -\n";  
-    traversePreorder(root);  
-    return 0;  
-}  
+// Getting started with tree
+#include <bits/stdc++.h>
+using namespace std;
+
+// binary tree
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Preorder Tree Traversal (root left right)
+void printPreorderTree(Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->key << " ";
+        printPreorderTree(root->left);
+        printPreorderTree(root->right);
+    }
+    return;
+}
+
+
+
+int main()
+{
+    // non empty tree
+    Node *root_two = new Node(10);
+    root_two->left = new Node(20);
+    root_two->right = new Node(30);
+
+    // defining value for sub node
+    root_two->left->left = new Node(40);
+    root_two->left->right = new Node(50);
+    root_two->left->right->left = new Node(70);
+    root_two->left->right->right = new Node(80);
+    root_two->right->right = new Node(60);
+    cout << "Preorder Tree Traversal" << endl;
+    printPreorderTree(root_two);
+    cout << endl;
+
+    return 0;
+}
